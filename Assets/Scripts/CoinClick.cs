eb5862374d88c79bd132ad33017b044f10715f7f
akkarugi <146344051+akkarugi@users.noreply.github.com>
2025-03-18 21:54:50 +0100

    transfer

--- Assets/Scripts/CoinClick.cs
+++ Assets/Scripts/CoinClick.cs
@@ -5,20 +5,27 @@ using TMPro;
 public class CoinClick : MonoBehaviour
 {
     public Button button;
+    public Button transferButton;
     public TMP_Text counterText;
+    public TMP_Text cryptoText;
 
-    private float counter = 0f;
+    [SerializeField] private double initialPoints = 0;
+    private double counter;
     private float lastClickTime = 0f;
     private const float DOUBLE_CLICK_THRESHOLD = 0.5f;
 
+    private int crypto = 0;
+    private int pointsNeeded = 10000;
+    private const int MAX_POINTS_NEEDED = 100000;
+    private const int POINTS_INCREMENT = 10000;
+
     void Start()
     {
-        if (button != null)
-        {
-            button.onClick.AddListener(OnButtonClick);
-        }
-
+        counter = initialPoints;
+        button.onClick.AddListener(OnButtonClick);
+        transferButton.onClick.AddListener(OnTransferButtonClick);
         UpdateCounterText();
+        UpdateCryptoText();
     }
 
     private void OnButtonClick()
@@ -27,23 +34,36 @@ public class CoinClick : MonoBehaviour
         float timeSinceLastClick = currentTime - lastClickTime;
 
         if (timeSinceLastClick < DOUBLE_CLICK_THRESHOLD)
-        {
-            counter += 1.5f;
-        }
+            counter += 1.5;
         else
-        {
-            counter += 1f;
-        }
+            counter += 1;
 
         lastClickTime = currentTime;
         UpdateCounterText();
     }
 
+    private void OnTransferButtonClick()
+    {
+        if (counter >= pointsNeeded)
+        {
+            counter -= pointsNeeded;
+            crypto++;
+            if (pointsNeeded < MAX_POINTS_NEEDED)
+                pointsNeeded += POINTS_INCREMENT;
+            UpdateCounterText();
+            UpdateCryptoText();
+        }
+    }
+
     private void UpdateCounterText()
     {
         if (counterText != null)
-        {
-            counterText.text = counter.ToString("F1");
-        }
+            counterText.text = $"{counter.ToString("F1")}/{pointsNeeded}";
+    }
+
+    private void UpdateCryptoText()
+    {
+        if (cryptoText != null)
+            cryptoText.text = $"Crypto: {crypto}";
     }
 }
